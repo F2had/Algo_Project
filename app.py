3fa9dea4fbda51b2f581f8458b3e6d805354150c
Fahad <Fahad.ali.331@gmail.com>
2020-04-12 07:15:14 -0400

    Added: Levenshtein distance algorithm to find similar strings.

--- app.py
+++ app.py
@@ -1,21 +1,9 @@
 from datetime import datetime
-from fuzzywuzzy import process
 import googlemaps
 import polyline
 from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
-
-
-# FIXME
-# change with the actual list of locations
-test_data = ['panorama', 'um', 'pantai hill park', 'pantai panorama', 'KK12', 'UM Cerntral']
-# take the user input for the query
-user_query = 'Pantai hi'
-
-
-def fuzzy(query, locations):
-    return process.extractOne(query, locations)[0]
 
 
 def compute_path(start, end):
