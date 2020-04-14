5350e2392a47a5f68a64f3f1419ec4b871738211
kyle <chennice456@gmail.com>
2020-04-14 22:50:06 +0800

    add more places and points and connect each of them

--- data/database.py
+++ data/database.py
@@ -1,36 +1,69 @@
 from .database_builder import get_latlon
 from .graph import GraphPoint, connect_points, MODE_BUS, MODE_WALKING, MODE_TRAIN
 
-points_names = [
+points_names =[
+    'Sunway Pyramid',
     'UM CENTRAL',
-    'Kuala Lumpur City Centre',
     'Mid Valley Megamall',
     'KL Sentral',
+    'National Mosque of Malaysia',
+    'Kuala Lumpur City Centre',
 
-    'KL Gateway',
+    #'Dataran Merdeka',
+    #'Menara Kuala Lumpur',
+    #'Kuala Lumpur International Airport',
+
+
+    'Masjid Al-Husna',
+    'Ppum Federal',
+    'KL 1102 Masjid Ar-Rahman UM, Lingkungan Budi'
+    'Mahsa University / Intan',
     'Lrt Station Universiti',
+    'KL Gateway',
+    'Pantai Panorama Condominium',
     'LRT Kerinchi',
     'LRT Abdullah Hukum',
-    'Pantai Panorama Condominium',
+    'Kuala Lumpur Station',
+
 ]
 
 points = {}
 
 # build the real points data collection
 for point_n in points_names:
-    latlon = list(get_latlon(point_n))
+    print(point_n)
+    latlon=list(get_latlon(point_n))
     points[point_n] = GraphPoint(point_n, latlon[0], latlon[1])
 
 # Connections building
-connect_points(points['UM CENTRAL'], points['KL Gateway'], MODE_BUS)
+
+
+#walking
+connect_points(points['Sunway Pyramid'],points['Masjid Al-Husna'],MODE_WALKING)
+connect_points(points['KL 1102 Masjid Ar-Rahman UM, Lingkungan Budi'],points['UM CENTRAL'],MODE_WALKING)
+connect_points(points['UM CENTRAL'],points['Mahsa University / Intan'],MODE_WALKING)
 connect_points(points['UM CENTRAL'], points['KL Gateway'], MODE_WALKING)
-connect_points(points['KL Gateway'], points['Lrt Station Universiti'], MODE_WALKING)
 connect_points(points['KL Gateway'], points['Pantai Panorama Condominium'], MODE_WALKING)
-connect_points(points['LRT Kerinchi'], points['Pantai Panorama Condominium'], MODE_WALKING)
+connect_points(points['Pantai Panorama Condominium'], points['Lrt Station Universiti'], MODE_WALKING)
+connect_points(points['Lrt Station Universiti'],points['LRT Kerinchi'],MODE_WALKING)
 connect_points(points['LRT Abdullah Hukum'], points['Mid Valley Megamall'], MODE_WALKING)
+connect_points(points['Kuala Lumpur Station'],points['National Mosque of Malaysia'],MODE_WALKING)
+connect_points(points['National Mosque of Malaysia'],points['Kuala Lumpur City Centre'],MODE_WALKING)
+connect_points(points['Kuala Lumpur Station'],points['Kuala Lumpur City Centre'],MODE_WALKING)
+
+# bus
+connect_points(points['UM CENTRAL'], points['KL Gateway'], MODE_BUS)
+connect_points(points['Lrt Station Universiti'],points['LRT Kerinchi'],MODE_BUS)
+connect_points(points['Mahsa University / Intan'],points['Mid Valley Megamall'],MODE_BUS)
+connect_points(points['Masjid Al-Husna'],points['Ppum Federal'],MODE_BUS)
+connect_points(points['Ppum Federal'],points['KL 1102 Masjid Ar-Rahman UM, Lingkungan Budi'],MODE_BUS)
+connect_points(points['Ppum Federal'],points['Mid Valley Megamall'],MODE_BUS)
+connect_points(points['Mid Valley Megamall'],points['KL Sentral'],MODE_BUS)
+
 
 # LRT train
 connect_points(points['Lrt Station Universiti'], points['LRT Kerinchi'], MODE_TRAIN)
 connect_points(points['LRT Kerinchi'], points['LRT Abdullah Hukum'], MODE_TRAIN)
 connect_points(points['LRT Abdullah Hukum'], points['KL Sentral'], MODE_TRAIN)
+connect_points(points['KL Sentral'],points['Kuala Lumpur Station'],MODE_TRAIN)
 connect_points(points['KL Sentral'], points['Kuala Lumpur City Centre'], MODE_TRAIN)
