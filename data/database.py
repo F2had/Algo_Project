47fcd9679835e9c1b1f27704e4fd71d750eb9d7c
Amjad Alsharafi <amjadsharafi10@gmail.com>
2020-04-14 17:51:00 +0800

    Fix: some errors in the names of the points

--- data/database.py
+++ data/database.py
@@ -10,7 +10,7 @@ points_names = [
     'KL Gateway',
     'Lrt Station Universiti',
     'LRT Kerinchi',
-    'Abdullah Hukum',
+    'LRT Abdullah Hukum',
     'Pantai Panorama Condominium',
 ]
 
@@ -18,7 +18,7 @@ points = {}
 
 # build the real points data collection
 for point_n in points_names:
-    latlon = get_latlon(point_n)
+    latlon = list(get_latlon(point_n))
     points[point_n] = GraphPoint(point_n, latlon[0], latlon[1])
 
 # Connections building
@@ -27,10 +27,10 @@ connect_points(points['UM CENTRAL'], points['KL Gateway'], MODE_WALKING)
 connect_points(points['KL Gateway'], points['Lrt Station Universiti'], MODE_WALKING)
 connect_points(points['KL Gateway'], points['Pantai Panorama Condominium'], MODE_WALKING)
 connect_points(points['LRT Kerinchi'], points['Pantai Panorama Condominium'], MODE_WALKING)
-connect_points(points['LRT Abdullah Hukum'], points['Mid Valley'], MODE_WALKING)
+connect_points(points['LRT Abdullah Hukum'], points['Mid Valley Megamall'], MODE_WALKING)
 
 # LRT train
 connect_points(points['Lrt Station Universiti'], points['LRT Kerinchi'], MODE_TRAIN)
 connect_points(points['LRT Kerinchi'], points['LRT Abdullah Hukum'], MODE_TRAIN)
 connect_points(points['LRT Abdullah Hukum'], points['KL Sentral'], MODE_TRAIN)
-connect_points(points['KL Sentral'], points['KLCC'], MODE_TRAIN)
+connect_points(points['KL Sentral'], points['Kuala Lumpur City Centre'], MODE_TRAIN)
